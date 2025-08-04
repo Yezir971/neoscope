ce9480c805901c574c2f608a6e67d6d0663a4d32
Yezir971 <james_ahmedaly@yahoo.com>
2025-08-04 17:49:27 +0200

    feat : add design footer and navbar

--- functions.php
+++ functions.php
@@ -5,5 +5,13 @@ if (! defined('WP_DEBUG')) {
 }
 
 add_action( 'wp_enqueue_scripts', function () {
-	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    // Charger les styles du thème parent
+    wp_enqueue_style( 'blocksy-parent-style', get_template_directory_uri() . '/style.css' );
+
+    // Charger les styles du thème enfant
+    wp_enqueue_style( 'blocksy-child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array('blocksy-parent-style'),
+        filemtime( get_stylesheet_directory() . '/style.css')
+    );
 });
